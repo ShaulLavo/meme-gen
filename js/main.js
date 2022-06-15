182ad25619d5702412a1c0263f83fa9cfe9bc5d1
shaul lavochkin <shaul919191@gmail.com>
2022-06-15 14:51:04 +0300

    fix after merge

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use-strict'
 
 const gImgs = []
@@ -32,54 +31,3 @@ function createMemeDb() {
 // 		}
 // 	]
 // }
-=======
-'use-strict'
-
-const gImgs = []
-const gImgCount = 16
-
-function init() {
-	createMemeDb()
-	renderMemeGallery()
-}
-
-function createMemeDb() {
-	for (let i = 0; i < gImgCount; i++) {
-		gImgs.push({
-			id: i,
-			url: `../../meme-imgs/${i}.jpg`,
-			keywords: ['funny', 'cat']
-		})
-	}
-}
-
-function renderMemeGallery() {
-	//TODO refactor this maybe w/o background
-
-	//add containers to gallery
-	const elGallery = document.querySelector('.img-gallery')
-	gImgs.forEach(img => {
-		elGallery.innerHTML += `<div class="img img${img.id}"></div>`
-	})
-
-	// add background image to containers
-	const elImgs = document.querySelectorAll('.img')
-	gImgs.forEach(img => {
-		elImgs[img.id].style.backgroundImage = `url(${img.url})`
-	})
-}
-
-// const gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
-// const gMeme = {
-// 	selectedImgId: 5,
-// 	selectedLineIdx: 0,
-// 	lines: [
-// 		{
-// 			txt: 'I sometimes eat Falafel',
-// 			size: 20,
-// 			align: 'left',
-// 			color: 'red'
-// 		}
-// 	]
-// }
->>>>>>> f474c49676bcd0727355cd3434d48b6de4646d1a
