f474c49676bcd0727355cd3434d48b6de4646d1a
Shaul Lavochkin <68500209+ShaulLavo@users.noreply.github.com>
2022-06-15 14:01:33 +0300

    even more dynamic

--- js/main.js
+++ js/main.js
@@ -16,13 +16,21 @@ function createMemeDb() {
 			keywords: ['funny', 'cat']
 		})
 	}
-	console.log(gImgs)
 }
 
 function renderMemeGallery() {
+	//TODO refactor this maybe w/o background
+
+	//add containers to gallery
+	const elGallery = document.querySelector('.img-gallery')
+	gImgs.forEach(img => {
+		elGallery.innerHTML += `<div class="img img${img.id}"></div>`
+	})
+
+	// add background image to containers
 	const elImgs = document.querySelectorAll('.img')
-	gImgs.forEach((img, idx) => {
-		elImgs[idx].style.backgroundImage = `url(${img.url})`
+	gImgs.forEach(img => {
+		elImgs[img.id].style.backgroundImage = `url(${img.url})`
 	})
 }
 
