f999d6055664dcf324db11245a67bfb36a6b2962
shaul lavochkin <shaul919191@gmail.com>
2022-06-15 18:01:22 +0300

    added edit modal layout

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -7,13 +7,19 @@ function onOpenEditor(id) {
 	initCanvas()
 	const elGallery = document.querySelector('.img-gallery ')
 	const elEditModal = document.querySelector('.edit-modal')
-	const img = getImageById(id)
-	const elImg = getImgEl(img)
-	renderImg(elImg)
+	renderMeme(id)
 	elGallery.style.display = 'none'
 	elEditModal.style.pointerEvents = 'auto'
 	elEditModal.style.opacity = '1'
 }
+
+function renderMeme(id) {
+	const img = getImageById(id)
+	const elImg = getImgEl(img)
+	renderImg(elImg)
+	renderImgTxt()
+}
+
 function getImgEl(img) {
 	const elImg = new Image()
 	elImg.src = img.url
@@ -33,4 +39,11 @@ function renderImg(img) {
 function initCanvas() {
 	gElCanvas = document.querySelector('.edit-modal canvas')
 	gCtx = gElCanvas.getContext('2d')
+}
+
+function renderImgTxt() {
+	gCtx.lineWidth = 2
+	gCtx.fillStyle = 'black'
+	gCtx.font = '50px Arial'
+	gCtx.fillText('Hello', 100, 100)
 }
