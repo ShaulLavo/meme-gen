7fcc19dcc3ee51c94c1bdc8c32e0d139d3f208e8
Shaul Lavochkin <68500209+ShaulLavo@users.noreply.github.com>
2022-06-15 13:46:26 +0300

    added dynamic image rendering and img DB

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 'use-strict'
 
-const gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
-const gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
-const gMeme = {
-	selectedImgId: 5,
-	selectedLineIdx: 0,
-	lines: [
-		{
-			txt: 'I sometimes eat Falafel',
-			size: 20,
-			align: 'left',
-			color: 'red'
-		}
-	]
+const gImgs = []
+const gImgCount = 16
+
+function init() {
+	createMemeDb()
+	renderMemeGallery()
 }
 
-function init() {}
+function createMemeDb() {
+	for (let i = 0; i < gImgCount; i++) {
+		gImgs.push({
+			id: i,
+			url: `../../meme-imgs/${i}.jpg`,
+			keywords: ['funny', 'cat']
+		})
+	}
+	console.log(gImgs)
+}
 
-function createMemeDb() {}
+function renderMemeGallery() {
+	const elImgs = document.querySelectorAll('.img')
+	gImgs.forEach((img, idx) => {
+		elImgs[idx].style.backgroundImage = `url(${img.url})`
+	})
+}
+
+// const gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
+// const gMeme = {
+// 	selectedImgId: 5,
+// 	selectedLineIdx: 0,
+// 	lines: [
+// 		{
+// 			txt: 'I sometimes eat Falafel',
+// 			size: 20,
+// 			align: 'left',
+// 			color: 'red'
+// 		}
+// 	]
+// }
