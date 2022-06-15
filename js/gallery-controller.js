3a2302838d1ff71f3205677f9e8343a02a1dadff
shaul lavochkin <shaul919191@gmail.com>
2022-06-15 16:05:34 +0300

    added edit modal

---
+++ js/gallery-controller.js
@@ -0,0 +1,17 @@
+'use-strict'
+
+function renderMemeGallery() {
+	//TODO refactor this maybe w/o background
+
+	//add containers to gallery
+	const elGallery = document.querySelector('.img-gallery')
+	gImgs.forEach(img => {
+		elGallery.innerHTML += `<div onclick='onOpenEditor(${img.id})' class="img img${img.id}"></div>`
+	})
+
+	// add background image to containers
+	const elImgs = document.querySelectorAll('.img')
+	gImgs.forEach(img => {
+		elImgs[img.id].style.backgroundImage = `url(${img.url})`
+	})
+}
--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,15 +1,36 @@
-function renderMemeGallery() {
-	//TODO refactor this maybe w/o background
+'use-strict'
 
-	//add containers to gallery
-	const elGallery = document.querySelector('.img-gallery')
-	gImgs.forEach(img => {
-		elGallery.innerHTML += `<div class="img img${img.id}"></div>`
-	})
+let gElCanvas
+let gCtx
 
-	// add background image to containers
-	const elImgs = document.querySelectorAll('.img')
-	gImgs.forEach(img => {
-		elImgs[img.id].style.backgroundImage = `url(${img.url})`
-	})
+function onOpenEditor(id) {
+	initCanvas()
+	const elGallery = document.querySelector('.img-gallery ')
+	const elEditModal = document.querySelector('.edit-modal')
+	const img = getImageById(id)
+	const elImg = getImgEl(img)
+	renderImg(elImg)
+	elGallery.style.display = 'none'
+	elEditModal.style.pointerEvents = 'auto'
+	elEditModal.style.opacity = '1'
+}
+function getImgEl(img) {
+	const elImg = new Image()
+	elImg.src = img.url
+	return elImg
+}
+
+function getImageById(id) {
+	return gImgs.find(img => img.id === id)
+}
+
+function renderImg(img) {
+	// if (!img) return
+	//Draw the img on the canvas
+	gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function initCanvas() {
+	gElCanvas = document.querySelector('.edit-modal canvas')
+	gCtx = gElCanvas.getContext('2d')
 }
